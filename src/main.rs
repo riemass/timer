977fd3d4e6c4eeea4c1f146ed5a366eaa9d9cf20
Samir Halilcevic <samir.halilcevic@marvelsoft.net>
2019-07-22 22:29:46 +0200

    Clean up code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-// #[allow(unused_imports)]
-// #[allow(dead_code)]
-use core::cmp::Reverse;
 use std::{
     collections::BinaryHeap,
     sync::{
@@ -12,73 +9,96 @@ use std::{
     time::{Duration, SystemTime},
 };
 
-#[derive(Eq, PartialEq, PartialOrd, Ord)]
+use std::cmp::{Ord, PartialEq, PartialOrd};
+
+#[derive(Eq)]
 struct Job {
     time: SystemTime,
     id: i32,
 }
 
+impl Ord for Job {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.time.cmp(&other.time).reverse()
+    }
+}
+
+impl PartialOrd for Job {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+}
+
+impl PartialEq for Job {
+    fn eq(&self, other: &Self) -> bool {
+        self.time == other.time
+    }
+}
+
+struct TimerInner {
+    jobs: Mutex<BinaryHeap<Job>>,
+    stop: AtomicBool,
+    condvar: Condvar,
+}
+
 struct Timer {
-    jobs: Arc<(Mutex<BinaryHeap<Reverse<Job>>>, AtomicBool, Condvar)>,
+    inner: Arc<TimerInner>,
     thread: std::thread::JoinHandle<()>,
 }
 
 impl Timer {
     pub fn new() -> Self {
-        let jobs = Arc::new((
-            Mutex::new(BinaryHeap::new()),
-            AtomicBool::new(false),
-            Condvar::new(),
-        ));
+        let inner = Arc::new(TimerInner {
+            jobs: Mutex::new(BinaryHeap::new()),
+            stop: AtomicBool::new(false),
+            condvar: Condvar::new(),
+        });
         let thread = {
-            let jobs = jobs.clone();
+            let inner = inner.clone();
             std::thread::spawn(move || {
-                Self::run(jobs);
+                Self::run(inner);
             })
         };
 
-        Self { jobs, thread }
+        Self { inner, thread }
     }
 
     pub fn add(&mut self, job: Job) {
-        let mut l = self.jobs.0.lock().unwrap();
-        l.push(Reverse(job));
-        self.jobs.2.notify_one();
+        let mut l = self.inner.jobs.lock().unwrap();
+        l.push(job);
+        self.inner.condvar.notify_one();
     }
 
-    fn run(jobs: Arc<(Mutex<BinaryHeap<Reverse<Job>>>, AtomicBool, Condvar)>) {
+    fn run(inner: Arc<TimerInner>) {
         loop {
-            let &(ref jobs, ref stop, ref condvar) = &*jobs;
-            let mut jobs_lock = jobs.lock().unwrap();
+            let mut jobs_lock = inner.jobs.lock().unwrap();
 
-            while jobs_lock.len() == 0 && !stop.load(Ordering::Relaxed) {
-                jobs_lock = condvar.wait(jobs_lock).unwrap();
+            while jobs_lock.len() == 0 && !inner.stop.load(Ordering::Relaxed) {
+                jobs_lock = inner.condvar.wait(jobs_lock).unwrap();
             }
-            if stop.load(Ordering::Relaxed) {
+            if inner.stop.load(Ordering::Relaxed) {
                 return;
             }
-            let now = SystemTime::now();
-            let job = jobs_lock.pop().unwrap().0;
-            if job.time > now {
-                let (jobs, wait) = condvar
-                    .wait_timeout(jobs_lock, job.time.duration_since(now).unwrap())
-                    .unwrap();
-                if stop.load(Ordering::Relaxed) {
+            let mut now = SystemTime::now();
+            let mut next_timestamp = jobs_lock.peek().unwrap().time;
+
+            while next_timestamp > now {
+                let wait = next_timestamp.duration_since(now).unwrap();
+                jobs_lock = inner.condvar.wait_timeout(jobs_lock, wait).unwrap().0;
+                if inner.stop.load(Ordering::Relaxed) {
                     return;
                 }
-                jobs_lock = jobs;
-                if !wait.timed_out() {
-                    jobs_lock.push(Reverse(job));
-                    continue;
-                }
+                next_timestamp = jobs_lock.peek().unwrap().time;
+                now = SystemTime::now();
             }
+            let job = jobs_lock.pop().unwrap();
             println!("Hello from {} at {:?}", job.id, SystemTime::now());
         }
     }
 
     pub fn stop(self) {
-        self.jobs.1.store(true, Ordering::SeqCst);
-        self.jobs.2.notify_one();
+        self.inner.stop.store(true, Ordering::SeqCst);
+        self.inner.condvar.notify_one();
         self.thread.join().unwrap();
     }
 }
