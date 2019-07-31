c6f6c2c66b1acf5df50e750e6ff139e10335a8e8
Samir Halilcevic <samir.halilcevic@marvelsoft.net>
2019-07-31 23:35:21 +0200

    Adds ability to schedule closures once or repeating

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,40 @@ use std::{
 
 use std::cmp::{Ord, PartialEq, PartialOrd};
 
-#[derive(Eq)]
+struct TaskOnce {
+    task: Box<dyn FnOnce() + Send>,
+}
+
+struct TaskRepeat {
+    task: Box<dyn FnMut() + Send>,
+    repeat: Duration,
+}
+
+enum Task {
+    Once(TaskOnce),
+    Repeat(TaskRepeat),
+}
+
+impl Task {
+    pub fn new_once<T>(task: T) -> Task
+    where T: FnOnce() + Send + 'static {
+        Task::Once(TaskOnce {
+            task: Box::new(task),
+        })
+    }
+
+    pub fn new_repeat<T>(task: T, repeat: Duration) -> Task
+    where T: FnMut() + Send + 'static {
+        Task::Repeat(TaskRepeat {
+            task: Box::new(task),
+            repeat,
+        })
+    }
+}
+
 struct Job {
     time: SystemTime,
-    id: i32,
+    task: Task,
 }
 
 impl Ord for Job {
@@ -34,6 +64,8 @@ impl PartialEq for Job {
         self.time == other.time
     }
 }
+
+impl Eq for Job {}
 
 struct TimerInner {
     jobs: Mutex<BinaryHeap<Job>>,
@@ -63,7 +95,23 @@ impl Timer {
         Self { inner, thread }
     }
 
-    pub fn add(&mut self, job: Job) {
+    pub fn schedule_once<J>(&mut self, time: SystemTime, job: J)
+    where J: FnOnce() + Send + 'static {
+        let job = Job {
+            time,
+            task: Task::new_once(job),
+        };
+        let mut l = self.inner.jobs.lock().unwrap();
+        l.push(job);
+        self.inner.condvar.notify_one();
+    }
+
+    pub fn schedule_repeat<J>(&mut self, time: SystemTime, job: J, repeat: Duration)
+    where J: FnMut() + Send + 'static {
+        let job = Job {
+            time,
+            task: Task::new_repeat(job, repeat),
+        };
         let mut l = self.inner.jobs.lock().unwrap();
         l.push(job);
         self.inner.condvar.notify_one();
@@ -71,28 +119,40 @@ impl Timer {
 
     fn run(inner: Arc<TimerInner>) {
         loop {
-            let mut jobs_lock = inner.jobs.lock().unwrap();
+            let mut job = {
+                let mut jobs_lock = inner.jobs.lock().unwrap();
 
-            while jobs_lock.len() == 0 && !inner.stop.load(Ordering::Relaxed) {
-                jobs_lock = inner.condvar.wait(jobs_lock).unwrap();
-            }
-            if inner.stop.load(Ordering::Relaxed) {
-                return;
-            }
-            let mut now = SystemTime::now();
-            let mut next_timestamp = jobs_lock.peek().unwrap().time;
-
-            while next_timestamp > now {
-                let wait = next_timestamp.duration_since(now).unwrap();
-                jobs_lock = inner.condvar.wait_timeout(jobs_lock, wait).unwrap().0;
+                while jobs_lock.len() == 0 && !inner.stop.load(Ordering::Relaxed) {
+                    jobs_lock = inner.condvar.wait(jobs_lock).unwrap();
+                }
                 if inner.stop.load(Ordering::Relaxed) {
                     return;
                 }
-                next_timestamp = jobs_lock.peek().unwrap().time;
-                now = SystemTime::now();
+
+                let mut now = SystemTime::now();
+                let mut next_timestamp = jobs_lock.peek().unwrap().time;
+                while next_timestamp > now {
+                    let wait = next_timestamp.duration_since(now).unwrap();
+                    jobs_lock = inner.condvar.wait_timeout(jobs_lock, wait).unwrap().0;
+                    if inner.stop.load(Ordering::Relaxed) {
+                        return;
+                    }
+                    next_timestamp = jobs_lock.peek().unwrap().time;
+                    now = SystemTime::now();
+                }
+                jobs_lock.pop().unwrap()
+            };
+            match job.task {
+                Task::Once(once_task) => {
+                    (once_task.task)();
+                },
+                Task::Repeat(ref mut repeat_task) => {
+                    (repeat_task.task)();
+                    job.time += repeat_task.repeat;
+                    let mut l = inner.jobs.lock().unwrap();
+                    l.push(job);
+                },
             }
-            let job = jobs_lock.pop().unwrap();
-            println!("Hello from {} at {:?}", job.id, SystemTime::now());
         }
     }
 
@@ -108,23 +168,25 @@ fn main() {
     let now = SystemTime::now();
     std::thread::sleep(Duration::from_millis(100));
 
-    let job = Job {
-        time: now - Duration::from_secs(1),
-        id: 1,
-    };
-    timer.add(job);
+    timer.schedule_once(now - Duration::from_secs(1), || {
+        println!("Hello1");
+    });
 
-    let job = Job {
-        time: now + Duration::from_secs(3),
-        id: 2,
-    };
-    timer.add(job);
+    timer.schedule_once(now + Duration::from_secs(3), || {
+        println!("Hello2");
+    });
 
-    let job = Job {
-        time: now + Duration::from_secs(1),
-        id: 3,
-    };
-    timer.add(job);
+    timer.schedule_once(now + Duration::from_secs(1), || {
+        println!("Hello3");
+    });
+
+    timer.schedule_repeat(
+        now + Duration::from_secs(1),
+        || {
+            println!("Repeat Hi");
+        },
+        Duration::from_millis(300),
+    );
 
     println!("Hello, world! Timer system starting at {:?}", now);
     std::thread::sleep(Duration::from_secs(5));
